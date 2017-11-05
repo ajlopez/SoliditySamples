275540539d648a6a2597ea589a908bf2c44814a1
ajlopez <webmaster@ajlopez.com>
2017-11-05 12:20:02 -0300

    Using .eth files for run DSL

--- simple/run.js
+++ simple/run.js
@@ -1,7 +1,18 @@
 
 var executor = require('./lib/executors').executor();
 
-var cmdfile = process.argv[2];
+var path = require('path');
+
+function normalize(filename) {
+	var ext = path.extname(filename);
+	
+	if (!ext || ext.length == 0)
+		return filename + '.eth';
+	
+	return filename;
+}
+
+var cmdfile = normalize(process.argv[2]);
 
 executor.executeFile(cmdfile, function (err, data) {
 	if (err)
