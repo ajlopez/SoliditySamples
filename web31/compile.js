2eb7279e32c8f0593c3105d3030d8f5736fbbad0
ajlopez <webmaster@ajlopez.com>
2017-11-12 17:58:45 -0300

    Compile script sample

---
+++ web31/compile.js
@@ -0,0 +1,23 @@
+
+var fs = require('fs');
+var solc = require('solc');
+
+function findImports(path) {
+    return { contents: fs.readFileSync('./' + path).toString() };
+    // return { error: 'File not found' }
+}
+
+function compileContract(filename) {
+    var input = fs.readFileSync(filename).toString();
+    var sources = {};
+    sources[filename] = input;
+	
+    var output = solc.compile({ sources: sources }, 1, findImports); // 1 activates the optimiser
+
+	return output.contracts;
+}
+
+var contracts = compileContract(process.argv[2]);
+
+console.dir(Object.keys(contracts));
+
