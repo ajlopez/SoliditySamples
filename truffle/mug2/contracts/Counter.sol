2fabf3a5a507928115b302f3623d804f78dd8c48
ajlopez <webmaster@ajlopez.com>
2018-09-17 19:34:26 -0300

    MUG 2 files first tests

--- truffle/mug2/contracts/Counter.sol
+++ truffle/mug2/contracts/Counter.sol
@@ -2,5 +2,17 @@ pragma solidity ^0.4.24;
 
 contract Counter {
     uint public counter;
+    
+    constructor() public {
+        counter = 1;
+    }
+    
+    function increment() public {
+        counter++;
+    }
+    
+    function add(uint val) public {
+        counter += val;
+    }
 }
 
--- truffle/mug2/test/Counter_test.js
+++ truffle/mug2/test/Counter_test.js
@@ -3,13 +3,35 @@ const Counter = artifacts.require('./Counter.sol');
 
 contract('Counter', function (accounts) {
     console.dir(accounts);
+    var counter;
     
-    it('first test', async function () {
-        const counter = await Counter.new();
-        
-        const c1 = await counter.counter();
-        
-        console.log('c1', c1.toNumber());
+    beforeEach(async function() {
+        counter = await Counter.new();
+    });
+
+    describe('first tests', function () {
+        it('initial counter', async function () {
+            const c1 = await counter.counter();
+            
+            assert.equal(c1, 1);
+        });
+
+        it('increment', async function () {
+            await counter.increment();
+            
+            const c1 = await counter.counter();
+            
+            assert.equal(c1, 2);
+        });
+
+        it('add', async function () {
+            await counter.increment();
+            await counter.add(40);
+            
+            const c1 = await counter.counter();
+            
+            assert.equal(c1, 42);
+        });
     });
 });
 
